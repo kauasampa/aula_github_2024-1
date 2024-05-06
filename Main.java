1584fa97fa25bcddf097fa245dcdd46a9634745f
“Luis” <mrqueiroz22014@gmail.com>
2024-05-06 18:36:25 -0300

    close issue #1: Adicionando classe Cliente e funcionalidade para cadastro de novos clientes na plataforma.

--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
 import java.util.Arrays;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
-		System.out.println("Fim");
-	}
-
+    public static void main(String[] args) {
+        Menu mainMenu = new Menu("Menu Principal", Arrays.asList("Cadastrar Cliente", "Listar Clientes", "Sair"));
+        boolean continuar = true;
+        while (continuar) {
+            int escolha = mainMenu.getSelection();
+            switch (escolha) {
+                case 1:
+                    mainMenu.cadastrarCliente();
+                    break;
+                case 2:
+                    mainMenu.listarClientes();
+                    break;
+                case 3:
+                    System.out.println("Fim");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        }
+    }
 }
--- Menu.java
+++ Menu.java
@@ -1,46 +1,55 @@
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-	private String title;
-	private List<String> options;
+    private String title;
+    private List<String> options;
+    private List<Cliente> clientes = new ArrayList<>();
 
-	public Menu(List<String> options) {
-		this.title = "Menu";
-		this.options = options;
-	}
+    public Menu(String title, List<String> options) {
+        this.title = title;
+        this.options = options;
+    }
 
-	public Menu(String title, List<String> options) {
-		this.title = title;
-		this.options = options;
-	}
+    public int getSelection() {
+        Scanner scanner = new Scanner(System.in);
+        int op = 0, i = 1;
+        while (op == 0) {
+            System.out.println(title + "\n");
+            for (String option : options) {
+                System.out.println(i++ + " - " + option);
+            }
+            System.out.println("Informe a opcao desejada.");
+            try {
+                op = Integer.parseInt(scanner.nextLine());
+                if (op < 1 || op >= i) {
+                    System.out.println("Opcao errada!");
+                    op = 0;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Por favor, digite um número válido.");
+            }
+        }
+        return op;
+    }
 
-	public int getSelection() {
-		int op = 0;
-		while (op==0){
-			System.out.println(title+"\n");
-			int i=1;
-			for (String option : options) {
-				System.out.println(i++ + " - " + option);
-			}
+    public void cadastrarCliente() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome do cliente:");
+        String nome = scanner.nextLine();
+        System.out.println("Digite o email do cliente:");
+        String email = scanner.nextLine();
+        System.out.println("Digite o telefone do cliente:");
+        String telefone = scanner.nextLine();
+        Cliente novoCliente = new Cliente(nome, email, telefone);
+        clientes.add(novoCliente);
+        System.out.println("Cliente cadastrado com sucesso!");
+    }
 
-			System.out.println("Informe a opcao desejada. ");
-			Scanner s = new Scanner(System.in);
-			String str = s.nextLine();
-			try {
-				op = Integer.parseInt(str);
-			}
-			catch (NumberFormatException e) {
-				op =0;
-			}
-			if (op>=i){
-				System.out.println("Opcao errada!");
-				op=0;
-			}
-
-		}
-		return op;
-	}
+    public void listarClientes() {
+        for (Cliente cliente : clientes) {
+            System.out.println(cliente);
+        }
+    }
 }
